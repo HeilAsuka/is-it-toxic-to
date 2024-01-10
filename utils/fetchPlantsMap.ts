7def7e5d24790c4881e20659a7d793fafd318904
HeilAsuka <heilasuka911@gmail.com>
2024-01-11 07:18:25 +0800

    Update fetchPlantsMap.ts

--- utils/fetchPlantsMap.ts
+++ utils/fetchPlantsMap.ts
@@ -4,7 +4,7 @@ import { getPlantKey } from "./plant-key-utils";
 
 export async function fetchPlantsMap(): Promise<Record<string, Plant>> {
   const res = await fetch(
-    "https://foldaway.github.io/toxic-plant-list-scraper/toxicPlants.json"
+    "https://raw.githubusercontent.com/HeilAsuka/is-it-toxic-to/main/toxicPlants_zh-Hans.json"
   );
   const plants = await res.json();
   const plantsMap = keyBy(plants, (plant: Plant) => getPlantKey(plant));
