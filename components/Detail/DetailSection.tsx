fe9edd1898a8e0c577b38f064fbff9757850a63d
HeilAsuka <heilasuka911@gmail.com>
2024-01-11 07:26:08 +0800

    Update DetailSection.tsx

--- components/Detail/DetailSection.tsx
+++ components/Detail/DetailSection.tsx
@@ -74,7 +74,7 @@ function _DetailSection(
               key={index}
               text={`${
                 ANIMAL_EMOJI_MAP[animal]
-              } ${animal[0].toUpperCase()}`}
+              } ${animal}`}
               intent="danger"
             />
           ))}
